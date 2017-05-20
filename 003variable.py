5a74b69124820235e0da7753e85187c65298be04
Murphy Zheng <murphy_zh@iCloud.com>
2017-05-20 20:37:20 +0800

    Add exercise file variable

---
+++ 003variable.py
@@ -0,0 +1,29 @@
+# "\"用来转义，输入特殊符号
+print('I\'m OK.')
+
+print('I\'m learning \nPython.')
+
+print('\\\n\\')
+
+# '''...'''表示多行内容
+print('''line1
+line2
+line3''')
+
+print(r'''line1
+line2
+line3''')
+
+# 变量
+a = 1
+
+t_007 = 't007'
+
+Answer = True
+
+# 等号=是赋值语句，可以把任意数据类型赋值给变量，同一个变量可以反复赋值，而且可## 以是不同类型的变量
+a = 111 # a是整数
+print(a)
+a = 'abc' # a变为字符串
+print(a)
+
