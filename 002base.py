a6137fcaba24e46d831ae90c9856efb3d7fd1088
Murphy Zheng <murphy_zh@iCloud.com>
2017-05-20 20:36:26 +0800

    Add exercise file of base

---
+++ 002base.py
@@ -0,0 +1,11 @@
+# print absolute value of an integeri
+
+a = int(input('please input an integer: '))
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+# Python使用缩进来组织代码块，请务必遵守约定俗成的习惯，坚持使用4个空格的缩进。
+
+# 在文本编辑器中，需要设置把Tab自动转换为4个空格，确保不混用Tab和空格。
