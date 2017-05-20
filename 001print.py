749c5d7e3129775d0f006e0043a5ce1909ba259e
Murphy Zheng <murphy_zh@iCloud.com>
2017-05-20 20:35:26 +0800

    Add exercise file of print

---
+++ 001print.py
@@ -0,0 +1,13 @@
+print ('Life is short','I use python.')
+
+print (300)
+
+print ('100 + 200 =',100 + 200)
+
+username = input()
+
+name = input('please input your name: ');print ("hello ",name)
+
+result = 1024 * 768
+
+print('1024 * 768 = ',result)
