f6301a2b55477cb828c514546d1b88f3ccb4ae9b
Murphy Zheng <murphy@mieasy.com>
2018-11-29 18:00:04 +0800

    Adjust directory structure

--- py_codes/015recur.py
+++ py_codes/015recur.py
@@ -3,7 +3,7 @@
 # 递归函数
 def fact(n):
     if n == 1:
-        return n;
+        return n
     return n * fact(n - 1)
 
 print('5! = ', fact(5))
--- py_codes/019generator.py
+++ py_codes/019generator.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 # 生成器(Generator)
-
