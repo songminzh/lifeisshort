caa1a09cab1a4e98b4d3025804e12c697b129e91
Murphy Zheng <murphy@mieasy.com>
2018-12-11 14:33:21 +0800

    Update 019generator.py

--- py_codes/019generator.py
+++ py_codes/019generator.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 
 # 生成器(Generator)
+
+L = [x * x for x in range(10)] # list
+
+g = (x * x for x in range(10)) # generator
+
+for n in g:
+    print(n)
+
+# 斐波拉契数列（Fibonacci）
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'Done'
+
+
+
+
+
+
+
