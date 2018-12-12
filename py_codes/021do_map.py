a28fddc30a2d4aaf2a8d1bf2a4e25b6e53962a5f
Murphy Zheng <murphy@mieasy.com>
2018-12-12 17:55:27 +0800

    Update 021

--- py_codes/021do_map.py
+++ py_codes/021do_map.py
@@ -4,3 +4,13 @@ def f(x):
     return x * x
 
 print(list(map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+
+
+# 将数组中的字符串变成首字母大写
+def normalize(name):
+    firstLetter = name[:1]
+    return firstLetter.upper() + name[1:]
+
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
