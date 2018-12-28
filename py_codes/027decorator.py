e19c1d328f7f679d650ba1861d5bca87708034ce
Murphy Zheng <murphy@mieasy.com>
2018-12-28 14:15:44 +0800

    Accomplisg

--- py_codes/027decorator.py
+++ py_codes/027decorator.py
@@ -36,7 +36,45 @@ print(today.__name__)
 # 设计一个decorator，作用于任何函数，并打印该函数的执行时间
 
 def metric(fn):
-    @functools.wraps(fn)
-    def counter()
-    print('%s executed in %s ms' % (fn.__name__, 10.24))
-    return fn
+    def decorator(fn):
+        @functools.wraps(fn)
+        def counter(*args, **kw):
+            start_time = time.time()
+            f = fn(*args, **kw)
+            end_time = time.time()
+            temp = end_time - start_time
+            print('%s executed in %s ms' % (fn.__name__, temp))
+            return f
+        return counter
+    return decorator
+
+# 测试
+@metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+
+# 测试
+@metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+
+if f == 33 and s == 7986:
+    print('测试成功')
+else:
+    print('测试失败')    
+
