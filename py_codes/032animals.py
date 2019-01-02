56a51d3283a4ff3e5731fbc6080a8e431ca7c3a5
Murphy Zheng <murphy@mieasy.com>
2019-01-02 17:15:55 +0800

    Create get_type.py

--- py_codes/032animals.py
+++ py_codes/032animals.py
@@ -2,4 +2,26 @@
 # -*- coding: utf-8 -*-
 # 继承与多态
 
-class Animal()
+class Animal(object):
+    def run(self):
+        print('Animal')
+
+class Dog(Animal):
+    def run(self):
+        print('Dog')
+    
+class Cat(Animal):
+    def run(self):
+        print('Cat')
+
+a = Animal()
+d = Dog()
+c = Cat()
+
+print('a is Animal?', isinstance(a, Animal))
+print('a is Dog?', isinstance(a, Dog))
+print('a is Cat?', isinstance(a, Cat))
+
+print('d is Animal?', isinstance(d, Animal))
+print('d is Dog?', isinstance(d, Dog))
+print('d is Cat?', isinstance(d, Cat))
