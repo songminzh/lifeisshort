2c8f85f5d28121a5de7ca3df5dae9fb939737362
Murphy Zheng <murphy@mieasy.com>
2019-01-02 17:24:01 +0800

    Update get_type.py

--- py_codes/033get_type.py
+++ py_codes/033get_type.py
@@ -1,2 +1,11 @@
 #!usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+print('type(123) is', type(123))
+print('type(\'123\') is', type('123'))
+print('type(None) is', type(None))
+print('type(abs) is', type(abs))
+
+import types
+
+print('type(\'abc\')is str?', type('abc')==str)
