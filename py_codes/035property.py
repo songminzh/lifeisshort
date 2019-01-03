967739c7002356c72eaa7c7fb6d82cccc45a7e9f
Murphy Zheng <murphy@mieasy.com>
2019-01-03 12:09:53 +0800

    Update property.py

--- py_codes/035property.py
+++ py_codes/035property.py
@@ -1,3 +1,33 @@
 #!usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+class Screen(object):
+    
+    @property
+    def width(self):
+        return self._width
+    
+    @property
+    def height(self):
+        return self._height
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+    
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def resolution(self):
+        return self._width * self._height
+
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
