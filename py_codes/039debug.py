29948b6ebb980f65fc46f9b1fada86c1df780fbb
Murphy Zheng <murphy@mieasy.com>
2019-01-18 17:08:08 +0800

    Update debug.py

--- py_codes/039debug.py
+++ py_codes/039debug.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# try
 try:
     print('try...')
     r = 10 / 0
@@ -10,3 +11,16 @@ except ZeroDivisionError as e:
 finally:
     print('finally...')
 print('END')
+
+
+# error
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    bar('0')
+
+main()
