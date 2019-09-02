96b27c3493e864252957ff94651475bbfb5ac6d7
郑敏 <laiyouxi4hao@zhengminde-Mac-mini.local>
2019-09-02 11:30:31 +0800

    Merge remote-tracking branch 'origin/master'

--- py_codes/040with_file.py
+++ py_codes/040with_file.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
+ 
+from datetime import datetime
+
+with open('test.txt', 'w') as f:
+    f.write('今天是')
+    dateNow = datetime.now().strftime('%Y-%m-%d')
+    f.write(dateNow)
+
+with open('test.txt', 'r') as f:
+    s = f.read()
+    print('open for read')
+    print(s)
+
+with open('test.txt', 'rb') as f:
+    s = f.read()
+    print('open as binary for read...')
+    print(s)
